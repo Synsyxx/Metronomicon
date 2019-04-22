5d14d95006454a3db9391799ef253673d0260dc7
Travis Simpson <travis.simpson92@hotmail.com>
2019-04-22 12:36:25 -0400

    added slider/button, initial styling

--- src/Metronomicon.js
+++ src/Metronomicon.js
@@ -3,7 +3,18 @@ import './Metronomicon.css';
 
 class Metronomicon extends Component {
     render() {
-        return <div className="metronome">hello</div>;
+        let bpm = 100; // initialize bpm variable at 100 beats per min
+        let playing = false; // prevent metronome from auto-start
+
+        return (
+            <div className="metronome">
+                <div className="bpm-slider">
+                    <div>{bpm} BPM</div>
+                    <input type="range" min="60" max="240" value={bpm} />
+                </div>
+                <button>{playing ? 'Stop' : 'Start'}</button> 
+            </div>
+        ); 
     }
 }
 
